5301a6f655f43f7f333862063ddf78fee08fe70e
simleo <simleo@crs4.it>
2015-01-23 12:21:20 +0100

    unit tests: added first actual test

--- src/test/java/it/crs4/features/AppTest.java
+++ src/test/java/it/crs4/features/AppTest.java
@@ -14,6 +14,9 @@ import loci.formats.meta.IMetadata;
 import loci.common.services.ServiceFactory;
 import loci.formats.services.OMEXMLService;
 
+import org.apache.avro.specific.SpecificDatumReader;
+import org.apache.avro.file.DataFileReader;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,8 +25,9 @@ public class AppTest extends TestCase {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
 
+  private boolean littleEndian = true;
   private int pixelType = FormatTools.UINT16;
-  private int seriesCount = 2;
+  private int seriesCount = 1;
   private int w = 512;
   private int h = 512;
   private int c = 1;
@@ -35,6 +39,17 @@ public class AppTest extends TestCase {
 
   private byte[][][] data;
 
+  //-- HACK HACK HACK --
+  private static String basename(String path) {
+    int i = path.lastIndexOf("/");
+    return (i < 0) ? path : path.substring(i + 1);
+  }
+
+  private static String stripext(String path) {
+    int i = path.lastIndexOf(".");
+    return (i < 0) ? path : path.substring(0, i);
+  }
+  //--------------------
 
   private byte[] makeImg() {
     byte[] img = new byte[size];
@@ -46,13 +61,14 @@ public class AppTest extends TestCase {
 
   private String makeImgFile() throws Exception {
     File target = File.createTempFile("pydoop_features_", ".ome.tiff");
+    target.deleteOnExit();
     String id = target.getAbsolutePath();
     String ptString = FormatTools.getPixelTypeString(pixelType);
     ServiceFactory factory = new ServiceFactory();
     OMEXMLService service = factory.getInstance(OMEXMLService.class);
     IMetadata meta = service.createOMEXMLMetadata();
     for (int s = 0; s < seriesCount; s++) {
-      MetadataTools.populateMetadata(meta, s, null, false, "XYZCT",
+      MetadataTools.populateMetadata(meta, s, null, littleEndian, "XYZCT",
         ptString, w, h, z, c, t, c);
     }
     IFormatWriter writer = new ImageWriter();
@@ -80,9 +96,25 @@ public class AppTest extends TestCase {
   }
 
   public void testApp() throws Exception {
-    assertTrue(true);  // FIXME
-    String fn = makeImgFile();
-    LOGGER.info("Created {}", fn);
+    String imgFn = makeImgFile();
+    LOGGER.info("Created {}", imgFn);
+    //-- dirty hack incoming --
+    App.main(new String[] {imgFn});
+    String avroFn = stripext(basename(imgFn)) + ".avro";
+    //-------------------------
+    File avroF = new File(avroFn);
+    avroF.setReadOnly();
+    DataFileReader<BioImgPlane> reader = new DataFileReader<BioImgPlane>(
+      avroF, new SpecificDatumReader<BioImgPlane>(BioImgPlane.class)
+    );
+    BioImgPlane p = null;
+    int planeIdx = 0;
+    while (reader.hasNext()) {
+      p = reader.next(p);
+      LOGGER.info("plane: {}[{}]", p.name, planeIdx);
+      planeIdx++;
+    }
+    assertTrue(planeIdx == planesCount);
   }
 
 }
