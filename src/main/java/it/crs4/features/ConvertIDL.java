bcdd631e8dbe699dcf78b32007fc3b233a2cecce
simleo <simleo@crs4.it>
2015-04-05 11:30:04 +0200

    fixed ConvertIDL

--- src/main/java/it/crs4/features/ConvertIDL.java
+++ src/main/java/it/crs4/features/ConvertIDL.java
@@ -1,10 +1,14 @@
 package it.crs4.features;
 
 import java.io.File;
+import java.io.FilenameFilter;
 import java.util.List;
 import java.util.ArrayList;
 
 import org.apache.avro.tool.IdlToSchemataTool;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 /**
@@ -12,6 +16,16 @@ import org.apache.avro.tool.IdlToSchemataTool;
  * http://www.virtualroadside.com/blog/index.php/2014/06/08/automatically-generating-avro-schemata-avsc-files-using-maven/
  */
 public class ConvertIDL {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(
+      ConvertIDL.class
+  );
+
+  private static class AvdlFilter implements FilenameFilter {
+    public boolean accept(File dir, String name) {
+      return name.toLowerCase().endsWith(".avdl");
+    }
+  }
 
   public static void main(String[] args) throws Exception {
     if (args.length < 2) {
@@ -21,8 +35,10 @@ public class ConvertIDL {
     File inDir = new File(args[0]);
     File outDir = new File(args[1]);
 
+    FilenameFilter filter = new AvdlFilter();
     IdlToSchemataTool tool = new IdlToSchemataTool();
-    for (File inFile: inDir.listFiles()) {
+    for (File inFile: inDir.listFiles(filter)) {
+      LOGGER.info("Processing {}", inFile.getName());
       List<String> toolArgs = new ArrayList<String>();
       toolArgs.add(inFile.getAbsolutePath());
       toolArgs.add(outDir.getAbsolutePath());
