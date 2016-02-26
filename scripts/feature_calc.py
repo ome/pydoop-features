bb2e872ba3f25f39533687926f56ca8f16bf9df3
simleo <simleo@crs4.it>
2016-02-26 16:05:14 +0000

    added support for long feature vectors

--- scripts/feature_calc.py
+++ scripts/feature_calc.py
@@ -2,13 +2,13 @@ from wndcharm.FeatureVector import FeatureVector
 from wndcharm.PyImageMatrix import PyImageMatrix
 
 
-def calc_features(img_arr, plane_tag):
+def calc_features(img_arr, plane_tag, long=False):
     assert len(img_arr.shape) == 2
     pychrm_matrix = PyImageMatrix()
     pychrm_matrix.allocate(img_arr.shape[1], img_arr.shape[0])
     numpy_matrix = pychrm_matrix.as_ndarray()
     numpy_matrix[:] = img_arr
-    signatures = FeatureVector(basename=plane_tag)
+    signatures = FeatureVector(basename=plane_tag, long=long)
     signatures.original_px_plane = pychrm_matrix
     signatures.GenerateFeatures(write_to_disk=False)
     return signatures
