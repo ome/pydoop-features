e7a33c014b12cf58c74e6079a075b9afbdba52ba
Simone Leo <simone.leo@gmail.com>
2016-03-21 14:13:58 +0000

    Revert "Single entry point"

--- docker/standalone/install_pydoop_features.sh
+++ docker/standalone/install_pydoop_features.sh
@@ -1,39 +1,31 @@
 set -ex
 
 . /etc/profile
-. ~/.bashrc
 
 set -u
 
-easy_install --user pip
-pip install --user avro
-pip install --user libtiff
+easy_install pip
 
-# clone dependencies with depth=1 to speed things up
-git clone --depth=1 --branch='metadata/merge/trigger' \
-    https://github.com/snoopycrimecop/bioformats.git
-git clone --depth=1 --branch='master' \
-    https://github.com/wnd-charm/wnd-charm.git
-
-git clone --branch='master' https://github.com/simleo/pydoop-features.git
+git clone https://github.com/openmicroscopy/bioformats.git
+git clone https://github.com/wnd-charm/wnd-charm.git
+git clone https://github.com/simleo/pydoop-features.git
 
 pushd bioformats
-# the Maven build needs to find a tag, but we've cut the history to depth 1
-git tag -a v5.1.8-METADATA-MERGE -m "tagging v5.1.8-METADATA-MERGE"
+git checkout metadata
 mvn install -DskipTests
 popd
 
 python set_bf_ver.py
+pip install avro
+pip install libtiff
 
 pushd wnd-charm
-touch aclocal.m4 Makefile.in configure config.h.in
-./configure --prefix="${HOME}"/.local
-make
+./build.sh
 make install
-python setup.py install --user
+python setup.py install
 popd
 
 pushd pydoop-features
 mvn clean compile assembly:single
-python setup.py install --user
+python setup.py install
 popd
