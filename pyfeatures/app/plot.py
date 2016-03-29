a7587741dd0d01f55fd6cbbae6887f03cc183f6a
simleo <simleo@crs4.it>
2016-03-29 17:07:14 +0100

    plot: can read from shelve and filter by feature sub-vector

--- pyfeatures/app/plot.py
+++ pyfeatures/app/plot.py
@@ -20,13 +20,18 @@
 Plot feature values across a given dimension.
 
 Reads feature data from Avro containers output by 'pyfeatures calc'.
+
+The '-f' option expects the name of a feature sub-vector, e.g.,
+'haralick_textures'.
 """
 
 import os
 import sys
 import errno
 import cPickle
+import shelve
 import warnings
+from contextlib import closing
 
 import matplotlib.pyplot as plt
 
@@ -44,32 +49,40 @@ AX_DESC = {
     "c": "Channels",
     "t": "Timepoints",
 }
+FV_NAMES = frozenset(_[0] for _ in FEATURE_NAMES.itervalues())
 
 
-def iter_records(f):
-    fn = getattr(f, "name", "")
-    if os.path.splitext(fn)[-1] == ".pickle":
-        for r in cPickle.load(f):
-            yield r
+def iter_records(fn):
+    ext = os.path.splitext(fn)[-1]
+    if ext == ".db":
+        with closing(shelve.open(fn, flag="r")) as shelf:
+            for r in shelf.itervalues():
+                yield r
+    elif ext == ".pickle":
+        with open(fn) as f:
+            for r in cPickle.load(f):
+                yield r
     else:
-        reader = AvroFileReader(f)
-        for r in reader:
-            yield r
+        with open(fn) as f:
+            reader = AvroFileReader(f)
+            for r in reader:
+                yield r
 
 
-def get_data(fn, axis):
+def get_data(fn, axis, feature=None):
     other_axes = [_ for _ in AXES if _ != axis]
     data = {}
-    with open(fn) as f:
-        for r in iter_records(f):
-            k1 = tuple(r[_] for _ in other_axes)
-            k2 = (r['x'], r['y'])
-            for name, idx in FEATURE_NAMES.itervalues():
-                k3 = "%s_%d" % (name, idx)
-                data.setdefault(
-                    k1, {}).setdefault(
-                        k2, {}).setdefault(
-                            k3, []).append((r['t'], r[name][idx]))
+    for r in iter_records(fn):
+        k1 = tuple(r[_] for _ in other_axes)
+        k2 = (r['x'], r['y'])
+        for name, idx in FEATURE_NAMES.itervalues():
+            if feature and name != feature:
+                continue
+            k3 = (name, idx)
+            data.setdefault(
+                k1, {}).setdefault(
+                    k2, {}).setdefault(
+                        k3, []).append((r['t'], r[name][idx]))
     for k1, v1 in data.iteritems():
         for k2, v2 in v1.iteritems():
             for k3, v3 in v2.iteritems():
@@ -77,13 +90,18 @@ def get_data(fn, axis):
     return data
 
 
-def plot_data(data, axis, out_dir):
+def plot_data(data, axis, out_dir, verbose=False):
     other_axes = [_ for _ in AXES if _ != axis]
     for k1, v1 in data.iteritems():
+        if verbose:
+            print "%r = %r" % (other_axes, k1)
         ax_tag = "%s%d_%s%d" % (other_axes[0], k1[0], other_axes[1], k1[1])
         for (x, y), v2 in v1.iteritems():
+            if verbose:
+                print "  (x, y) = (%d, %d)" % (x, y)
             xy_tag = "x%d_y%d" % (x, y)
-            for feat_tag, feat_values in v2.iteritems():
+            for (name, idx), feat_values in v2.iteritems():
+                feat_tag = "%s_%d" % (name, idx)
                 plt.close("all")
                 fig, ax = plt.subplots()
                 x = range(len(feat_values))
@@ -100,7 +118,10 @@ def add_parser(subparsers):
                         help="Avro file containing feature data")
     parser.add_argument("axis", metavar="|".join(AXES), choices=AXES,
                         help="what to map to the horizontal axis in the plots")
+    parser.add_argument("-f", "--feature", metavar="FEATURE", choices=FV_NAMES,
+                        help="select only this feature (sub-vector)")
     parser.add_argument('-o', '--out-dir', metavar='DIR', default=os.getcwd())
+    parser.add_argument('-v', '--verbose', action="store_true")
     parser.set_defaults(func=run)
     return parser
 
@@ -111,5 +132,7 @@ def run(args, extra_argv=None):
     except OSError as e:
         if e.errno != errno.EEXIST:
             sys.exit('Cannot create output dir: %s' % e)
-    data = get_data(args.in_fn, args.axis)
-    plot_data(data, args.axis, args.out_dir)
+    if args.verbose:
+        print "getting data..."
+    data = get_data(args.in_fn, args.axis, feature=args.feature)
+    plot_data(data, args.axis, args.out_dir, verbose=args.verbose)
