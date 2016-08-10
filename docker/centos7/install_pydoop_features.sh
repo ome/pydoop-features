3c8b762b13f815f80e189c6eadbcdbd67c7604d7
Simon Li <spli@dundee.ac.uk>
2016-08-10 16:41:15 +0100

    Add centos:7 Dockerfile

--- docker/centos7/install_pydoop_features.sh
+++ docker/centos7/install_pydoop_features.sh
@@ -4,6 +4,9 @@ set -ex
 
 set -u
 
+git config --get user.email || git config --global user.email ome-devel@lists.openmicroscopy.org.uk
+git config --get user.name || git config --global user.name omedev
+
 curl -o get-pip.py https://bootstrap.pypa.io/get-pip.py
 python get-pip.py
 pip install avro libtiff matplotlib
